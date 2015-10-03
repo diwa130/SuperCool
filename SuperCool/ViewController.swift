a67d166b2581af3ad50bc562978860b21a001cd1
Walter <walter@WALTER-MacBook-Air.local>
2015-10-02 21:40:12 -0400

    initial version

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Walter on 9/25/15.
+//  Copyright © 2015 diwa130. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBG: UIImageView!
+    
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUnCool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBG.hidden = false
+        uncoolButton.hidden = true
+        
+        
+    }
+
+}
+
